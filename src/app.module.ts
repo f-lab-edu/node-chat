801d877bf54d7f72daf1fc563de27466369d909b
Cein1 <k.heeyeon22@gmail.com>
2025-05-17 02:13:51 +0900

    poc(redis): NestJS의 microservices 활용한 redis Pub/Sub 기능 사용
    - NestJS microservices 라이브러리 활용한 redis Pub/Sub 기능 사용하여 poc 진행
    - process.env 대신 NestJS ConfigModule 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsModule } from './events/events.module';
+import { RedisClientModule } from './redis/redis.client.module';
+import { RedisModule } from './redis/redis.module';
 
 @Module({
-  imports: [EventsModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['./config/.development.env'],
+      isGlobal: true,
+    }),
+    EventsModule,
+    RedisModule,
+    RedisClientModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
