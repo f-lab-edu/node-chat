9fc09be112a4fad155b2777689bc1539e17c8f6e
Cein1 <k.heeyeon22@gmail.com>
2025-05-31 17:27:06 +0900

    feat(users): 사용자 등록하기 with uuidv7

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { v7 as uuidv7 } from 'uuid';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private primsaService: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const userExist = await this.primsaService.uSR.findFirst({
+      where: { USR_EMAIL: createUserDto.email },
+    });
+
+    if (userExist) throw Error('the user already exists');
+
+    return await this.primsaService.uSR.create({
+      data: {
+        USR_UUID: uuidv7(),
+        USR_NAME: createUserDto.name,
+        USR_EMAIL: createUserDto.email,
+      },
+    });
   }
 
   findAll() {
